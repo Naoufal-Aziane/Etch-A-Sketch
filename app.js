1264c3f0dc59cd9acd809870db1104c18cb5e819
Naoufal Aziane <naoufal.aziane@pm.me>
2022-06-26 19:26:32 +0100

    Select the UI variables form the dom

---
+++ app.js
@@ -0,0 +1,6 @@
+// Select the UI variables form the dom
+const rangeNum = document.querySelectorAll('.range-num');
+const rangeInput = document.querySelector('.input-range');
+const colorPicker = document.querySelector('.color-picker');
+const gridContainer = document.querySelector('.grid-container');
+
