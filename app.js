145b94ac94963dc779399d17929f2c068f632b88
Naoufal Aziane <naoufal.aziane@pm.me>
2022-06-26 20:15:50 +0100

    Create function to change the divs color when hover in the grid layout

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 // Select the UI variables form the dom
 const rangeNum = document.querySelectorAll('.range-num');
 const rangeInput = document.querySelector('.input-range');
-const colorPicker = document.querySelector('.color-picker');
 const gridContainer = document.querySelector('.grid-container');
 let pixel = rangeNum[0].textContent;
 const divWidth = parseInt(window.getComputedStyle(gridContainer).getPropertyValue('width')) / pixel;
 // Create pixelGenerator() function
 
-function pixelGenerator(pixel) {
+function pixelGenerator (pixel) {
   for(let i = 0 ; i<pixel*pixel ;i++){
     gridContainer.style.gridTemplateColumns = `repeat(${pixel},1fr)`
   const div = document.createElement('div');
@@ -17,3 +16,20 @@ function pixelGenerator(pixel) {
   gridContainer.appendChild(div);
   }
 }
+
+function pixelColorChanger (){
+const divs = document.querySelectorAll('.grid-container div');
+  divs.forEach((div) => div.addEventListener('mouseover',function() { 
+    let color = document.querySelector('.color-picker').value;
+      this.style.backgroundColor = color
+    }))
+}
+
+
+
+
+// Create the sketch grid layout when the dom load
+window.addEventListener('DOMContentLoaded',()=> {
+  pixelGenerator(pixel);
+  pixelColorChanger();
+});
