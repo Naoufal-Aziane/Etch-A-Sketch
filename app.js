fef8921d1fba40b1f1777e4ebde8489ae808e12b
Naoufal Aziane <naoufal.aziane@pm.me>
2022-06-26 19:50:08 +0100

    Create function to generate 16x16 grid layout

--- app.js
+++ app.js
@@ -3,4 +3,17 @@ const rangeNum = document.querySelectorAll('.range-num');
 const rangeInput = document.querySelector('.input-range');
 const colorPicker = document.querySelector('.color-picker');
 const gridContainer = document.querySelector('.grid-container');
+let pixel = rangeNum[0].textContent;
+const divWidth = parseInt(window.getComputedStyle(gridContainer).getPropertyValue('width')) / pixel;
+// Create pixelGenerator() function
 
+function pixelGenerator(pixel) {
+  for(let i = 0 ; i<pixel*pixel ;i++){
+    gridContainer.style.gridTemplateColumns = `repeat(${pixel},1fr)`
+  const div = document.createElement('div');
+  div.style.width = `${divWidth}`;
+  div.style.height = `${divWidth}`;
+  div.style.backgroundColor = '#aa9f9f';
+  gridContainer.appendChild(div);
+  }
+}
